366fd2fd0292448c7d120e5b981dec07ce621653
nicolerodriguezsmc <rodriguez_nicole_l01@student.smc.edu>
2025-07-06 23:39:27 -0700

    Update myDataJournal.js
    create weekData array with example data

--- myDataJournal.js
+++ myDataJournal.js
@@ -4,3 +4,14 @@
 * Module 4b Assignment
 * URL: https://github.com/nicolerodriguezsmc/cs81-module4b-mydataexplorer
 */
+
+// Weekly data journal
+let weekData = [
+  { day: "Monday", sleepHours: 7, screenTime: 5, mood: "happy", caffeineIntake: 2, focusLevel: 7 },
+  { day: "Tuesday", sleepHours: 6, screenTime: 7, mood: "productive", caffeineIntake: 3, focusLevel: 6 },
+  { day: "Wednesday", sleepHours: 8, screenTime: 8, mood: "tired", caffeineIntake: 1, focusLevel: 5 },
+  { day: "Thursday", sleepHours: 7.5, screenTime: 6, mood: "productive", caffeineIntake: 2, focusLevel: 8 },
+  { day: "Friday", sleepHours: 6, screenTime: 4, mood: "happy", caffeineIntake: 2, focusLevel: 7 },
+  { day: "Saturday", sleepHours: 9, screenTime: 3, mood: "relaxed", caffeineIntake: 0, focusLevel: 4 },
+  { day: "Sunday", sleepHours: 8, screenTime: 5, mood: "productive", caffeineIntake: 1, focusLevel: 6 }
+];
